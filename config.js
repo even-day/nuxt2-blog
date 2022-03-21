3f048b373ec98a135b69415d3b82e3ef15f9b738
even-day <102035685+even-day@users.noreply.github.com>
2022-03-21 21:45:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'yun yu yuan',
-  githubName: 'yunyuyuan',
+  title: 'shu sheng',
+  githubName: 'even-day',
   githubRepo: 'nuxt2-blog',
-  githubBranch: 'master',
+  githubBranch: 'blog',
   domain: 'https://blog.halberd.cn',
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: '书生的博客，书写值得书写的东西',
+  SEO_keywords: 'shusheng,shusheng\'s blog,书生,书生的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
