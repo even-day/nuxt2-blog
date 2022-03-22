e6ac7c6c6c721d0e97acfc33e5a2c5776b24cb8b
HA <10175803+aguny@user.noreply.gitee.com>
2022-03-22 22:38:46 +0800

    更新

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'shu sheng',
+  title: 'even day',
   githubName: 'even-day',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'blog',
-  domain: 'https://blog.halberd.cn',
-  SEO_description: '书生的博客，书写值得书写的东西',
-  SEO_keywords: 'shusheng,shusheng\'s blog,书生,书生的博客',
+  domain: 'https://blog-45c.pages.dev',
+  SEO_description: 'even-day的博客，书写值得书写的东西',
+  SEO_keywords: 'even-day,even-day\'s blog,even-day,even-day的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
