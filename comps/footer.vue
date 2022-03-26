1a07fe3ac74d65bca974708581dadc86b0c02bb9
even-day <102035685+even-day@users.noreply.github.com>
2022-03-26 11:43:29 +0800

    Update footer.vue

--- comps/footer.vue
+++ comps/footer.vue
@@ -2,7 +2,7 @@
   <footer id="footer" class="flex w100">
     <div class="middle flexc">
       <span>Copyright (c) 2022-2022 <b><a target="_blank" :href="'https://github.com/'+user">even-day</a> | {{ hostName }}</b></span>
-      <span class="flex"><a class="rss" target="_blank" href="/sitemap.xml" title="rss">RSS <svg-icon name="rss"/></a>| Powered By <a class="nuxt" href="https://nuxtjs.org/" target="_blank">Nuxtjs</a>| 已运行<span id='days'></span>天</span>
+      <span class="flex"><a class="rss" target="_blank" href="/sitemap.xml" title="rss">RSS <svg-icon name="rss"/></a>| Powered By <a class="nuxt" href="https://nuxtjs.org/" target="_blank">Nuxtjs</a></span>
     </div>
   </footer>
 </template>
@@ -10,15 +10,6 @@
 <script>
 import {inBrowser} from "~/utils/utils";
 import config from "@/config";
-
-if (process.client) {
-var s1 = '2022-03-22';//设置为你的建站时间
-s1 = new Date(s1.replace(/-/g, "/"));
-var s2 = new Date();
-var days = s2.getTime() - s1.getTime();
-var number_of_days = parseInt(days / (1000 * 60 * 60 * 24));
-document.getElementById("days").innerHTML = number_of_days;
-}
 
 export default {
   name: "the-footer",
