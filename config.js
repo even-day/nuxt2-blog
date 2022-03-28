3eb28d06546fdf5e97fd72cabb8068dba60a4aff
HA <10175803+aguny@user.noreply.gitee.com>
2022-03-28 22:33:02 +0800

    更新

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export default {
-  title: 'even day',
+  title: 'even',
   githubName: 'even-day',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'blog',
-  domain: 'https://blog-45c.pages.dev',
+  domain: 'https://blog-even-day.vercel.app',
   SEO_description: 'even-day的博客，书写值得书写的东西',
   SEO_keywords: 'even-day,even-day\'s blog,even-day,even-day的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
